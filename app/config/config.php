d827765e823ad63aab9500ab363c752c6873c60a
Marcus <marcusviniciussolleh@gmail.com>
2024-12-20 17:36:29 -0300

    Estrutura do Framework

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('BASE','/framework/');
+define('UNSET_URI_COUNT', 1);
+define('DEBUG_URI', false);
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','framework');
+
+?>
